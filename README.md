622890fbbd5e66008004aace99caccf43caebfbc
Barrett Clark <barrett@websitesbybarrett.com>
2015-07-10 12:46:59 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Language Comparison - Go Client
+
+A Go client implementation that parses the simple JSON output from the rack server.
+
+Go is a little whacky in that it has an opinion about where you put your code.  See https://golang.org/ for setup info.
+
+## Run
+
+    go run client.go
---
+++ client.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+type Payload struct {
+	Name          string    `json:"name"`
+	Pi            float64   `json:"pi"`
+	TheBestNumber int       `json:"best_number"`
+	RightNow      time.Time `json:"right_now"`
+}
+
+func payload() (*Payload, error) {
+	req, _ := http.NewRequest("GET", "http://localhost:9292", nil)
+	req.Header.Set("Accepts", "application/json")
+	client := &http.Client{}
+	resp, _ := client.Do(req)
+	defer resp.Body.Close()
+	content, _ := ioutil.ReadAll(resp.Body)
+
+	// Parse the response
+	var payload Payload
+	err := json.Unmarshal(content, &payload)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	return &payload, nil
+}
+
+func main() {
+	payload, _ := payload()
+	fmt.Printf("%+v\n", payload)
+}
